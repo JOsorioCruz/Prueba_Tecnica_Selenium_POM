966efbabbdba26d8aef4d8b6b2f6479b94da8978
JairoEstudiante <josorioc@unicartagena.edu.co>
2024-08-06 00:09:59 -0500

    /se agrega un metodo de validacion en el inicio de sesion

--- src/test/java/pages/InicioLoginPage.java
+++ src/test/java/pages/InicioLoginPage.java
@@ -2,21 +2,22 @@ package pages;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import credenciales.Usuarios;
 import org.openqa.selenium.By;
 import url.Prod;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class InicioLoginPage extends BasePage {
-
-    Usuarios crendenciales = new Usuarios();
     Prod url1 = new Prod();
 
-    private SelenideElement  botonMakeApp = $(By.id("btn-make-appointment"));
+    private SelenideElement botonMakeApp = $(By.id("btn-make-appointment"));
     private SelenideElement campoUsuario = $(By.xpath("//input[@id='txt-username']"));
     private SelenideElement campoContrasena = $(By.xpath("//div/input[@id='txt-password']"));
     private SelenideElement botonLogin = $(By.id("btn-login"));
+    private SelenideElement textoInicioSesionvalida = $(By.linkText("Make Appointment"));
+    private SelenideElement mensajeDeErrorInicioSesion = $(By.linkText("credenciales.Login failed! Please ensure the username and password are valid."));
+
     public void entrarAlPagina(){
         navegador(url1.urlPruebas());
     }
@@ -25,15 +26,26 @@ public class InicioLoginPage extends BasePage {
         Selenide.sleep(3000);
     }
 
-    public void escribirUsuario(){
-        campoUsuario.sendKeys(crendenciales.getUsuario());
+    public void escribirUsuario(String text){
+        campoUsuario.sendKeys(text);
     }
 
-    public void escribirContrasena(){
-        campoContrasena.sendKeys(crendenciales.getContrasena());
+    public void escribirContrasena(String text){
+        campoContrasena.sendKeys(text);
     }
 
     public void hacerClickEnBotonLogin(){
         botonLogin.click();
+    }
+
+    public void textoVisible(){
+        textoInicioSesionvalida.shouldBe(visible);
+    }
+    public void textoVisibleDos(){
+        mensajeDeErrorInicioSesion.shouldNotBe(visible);
+    }
+
+    public void textoNoVisible(){
+        textoInicioSesionvalida.shouldNotBe(visible.negate());
     }
 }
